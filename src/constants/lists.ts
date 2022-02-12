69bedc56e94bc5f369ebdb73a8bc78a249162398
top1st <riguang102@gmail.com>
2022-02-13 01:25:36 +0800

    Replace Default Token LIst

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -6,13 +6,13 @@ const CMC_ALL_LIST = 'defi.cmc.eth'
 const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
-const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
+// const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
 const KLEROS_LIST = 't2crtokens.eth'
 export const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json'
-const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
 const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
 const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
+const TOP_LIST = 'https://raw.githubusercontent.com/top1st/uniswap-tokenlist/main/top.tokenlist.json'
 
 // only load blocked list if on app url
 export const UNSUPPORTED_LIST_URLS: string[] = IS_ON_APP_URL ? [BA_LIST] : []
@@ -26,13 +26,12 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   UMA_LIST,
   WRAPPED_LIST,
   SET_LIST,
-  ROLL_LIST,
   COINGECKO_LIST,
   KLEROS_LIST,
   OPTIMISM_LIST,
-  GEMINI_LIST,
+  TOP_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [GEMINI_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [TOP_LIST]
